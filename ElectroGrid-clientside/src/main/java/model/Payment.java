19eed087515f68015491191c00b5e4639e407be0
prabhaviweerasinghe <prabhavipanchali0709@gmail.com>
2022-05-14 00:44:30 +0530

    set DB connection

--- ElectroGrid-clientside/src/main/java/model/Payment.java
+++ ElectroGrid-clientside/src/main/java/model/Payment.java
@@ -1,5 +1,26 @@
 package model;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class Payment {
 
+	// A common method to connect to the DB
+		private Connection connect() {
+			Connection con = null;
+
+			try {
+				Class.forName("com.mysql.jdbc.Driver");
+				// Provide the correct details: DBServer/DBName, username, password
+				con = DriverManager.getConnection("jdbc:mysql://localhost:3306/electrogrid_clientside", "root", "");
+
+				// For testing
+				System.out.print("Successfully connected");
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+			return con;
+		}
 }
