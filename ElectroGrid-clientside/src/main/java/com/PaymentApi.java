ee61239379d7d9065d4e7fe9d679561c800a8f05
prabhaviweerasinghe <prabhavipanchali0709@gmail.com>
2022-05-15 00:14:14 +0530

    do get method

--- ElectroGrid-clientside/src/main/java/com/PaymentApi.java
+++ ElectroGrid-clientside/src/main/java/com/PaymentApi.java
@@ -1,41 +1,59 @@
 package com;
 
 import java.io.IOException;
+
+
+
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class PaymentApi
- */
+import model.Payment;
+
 @WebServlet("/PaymentApi")
 public class PaymentApi extends HttpServlet {
+	
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
+	
+	Payment paymentObj = new Payment();
+	
     public PaymentApi() {
         super();
         // TODO Auto-generated constructor stub
     }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse 
+	 * response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *  response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		
+		String result = paymentObj.insertPayment(request.getParameter("Amount"),
+				request.getParameter("PaymentCardNo"), request.getParameter("PaymentType"), 
+				request.getParameter("PaymntDate"));
+		
+		response.getWriter().write(result);
 	}
 
+	
+	
+	
 }
