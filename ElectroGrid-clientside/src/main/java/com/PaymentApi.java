441728b47562e0bf5dce235c31b28c9956e9188c
prabhaviweerasinghe <prabhavipanchali0709@gmail.com>
2022-05-15 00:18:05 +0530

    Do put method

--- ElectroGrid-clientside/src/main/java/com/PaymentApi.java
+++ ElectroGrid-clientside/src/main/java/com/PaymentApi.java
@@ -1,9 +1,9 @@
 package com;
 
 import java.io.IOException;
-
-
-
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -53,7 +53,41 @@ public class PaymentApi extends HttpServlet {
 		response.getWriter().write(result);
 	}
 
+	/**
+	 * @see HttpServlet#doPut(HttpServletRequest, HttpServletResponse)
+	 */
 	
+	private Map<String, String> getParasMap(HttpServletRequest request) {
+		Map<String, String> map = new HashMap<String, String>();
+		try {
+			Scanner scanner = new Scanner(request.getInputStream(), "UTF-8");
+			String queryString = scanner.hasNext() ? scanner.useDelimiter("\\A").next() : "";
+			scanner.close();
+
+			String[] params = queryString.split("&");
+			for (String param : params) {
+				String[] p = param.split("=");
+				map.put(p[0], p[1]);
+			}
+
+		} catch (Exception e) {
+
+		}
+		return map;
+	}
+
+	protected void doPut(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		Map<String, String> param = getParasMap(request);
+
+		String result = paymentObj.updatePayment(param.get("hidPaymentIDSave").toString(),
+				param.get("Amount").toString().toString().replace("+", " "),
+				param.get("PaymentCardNo").toString().toString().replace("+", " "),
+				param.get("PaymentType").toString().toString().replace("+", " "), param.get("PaymentDate").toString());
+
+		response.getWriter().write(result);
+	}
 	
 	
 }
