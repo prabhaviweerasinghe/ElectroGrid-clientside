eec8b99f16bb1f345901c702b271a7bbd5b0ab9e
prabhaviweerasinghe <prabhavipanchali0709@gmail.com>
2022-05-15 00:32:47 +0530

    Do payment save

---
+++ ElectroGrid-clientside/WebContent/components/pay.js
@@ -0,0 +1,106 @@
+$(document).ready(function() 
+{  
+	if ($("#alertSuccess").text().trim() == "")  
+	{   
+		$("#alertSuccess").hide();  
+	} 
+	$("#alertError").hide(); 
+});
+
+//SAVE =========================================
+$(document).on("click", "#btnSave", function(event) 
+{  
+	// Clear alerts---------------------  
+	$("#alertSuccess").text("");  
+	$("#alertSuccess").hide();  
+	$("#alertError").text("");  
+	$("#alertError").hide(); 
+
+	// Form validation-------------------  
+	var status = validatePaymentForm();  
+	if (status != true)  
+	{   
+		$("#alertError").text(status);   
+		$("#alertError").show();   
+		return;  
+	} 
+
+	// If valid------------------------  
+	var t = ($("#hidPaymentIDSave").val() == "") ? "POST" : "PUT";
+	
+	$.ajax(
+	{
+		url : "PaymentApi",
+		type : t,
+		data : $("#formPayment").serialize(),
+		dataType : "text",
+		complete : function(response, status)
+		{
+			onPaymentSaveComplete(response.responseText, status);
+		}
+	});
+}); 
+
+function onPaymentSaveComplete(response, status){
+	if(status == "success")
+	{
+		var resultSet = JSON.parse(response);
+			
+		if(resultSet.status.trim() == "success")
+		{
+			$("#alertSuccess").text("Successfully Saved.");
+			$("#alertSuccess").show();
+					
+			$("#divItemsGrid").html(resultSet.data);
+	
+		}else if(resultSet.status.trim() == "error"){
+			$("#alertError").text(resultSet.data);
+			$("#alertError").show();
+		}
+	}else if(status == "error"){
+		$("#alertError").text("Error While Saving.");
+		$("#slertError").show();
+	}else{
+		$("#alertError").text("Unknown Error while Saving.");
+		$("#alertError").show();
+	}
+	$("#hidPaymentIDSave").val("");
+	$("#formPayment")[0].reset();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * 
+ */
